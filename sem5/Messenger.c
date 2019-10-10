c7a69c8387d5bc9a24a5ff735231147241f5da24
Shkut Maxim <43554469+MXMSHKT@users.noreply.github.com>
2019-10-10 19:00:21 +0300

    Update Messenger.c

--- sem5/Messenger.c
+++ sem5/Messenger.c
@@ -8,31 +8,22 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-void Dia(char *name01, char *name10, int fd){
-    int frk = fork();
-    while(1){
-        if (frk == 0){
-            fd = open(name10, O_WRONLY);
-            char buf[100];
-            memset(buf, 0, 100);
-            fgets(buf, 100, stdin);
-            write(fd, buf, 100);
-            memset(buf, 0, 100);
-            close(fd);
-        }
-        else{
+void ReadFifo(char* file){
+    char output[100];
+    int fd = open(file, O_RDONLY);
+    read(fd, output, 100);
+    printf("%s", output);
+    close(fd);
+}
 
-            fd = open(name01, O_RDONLY);
-            char buf[100];
-            memset(buf, 0, 100);
-            read(fd, buf, 100);
-            printf("%s", buf);
-            memset(buf, 0, 100);
-            close(fd);
-        }
-    }
-    kill(getpid(), SIGKILL);
-}      
+void WriteFifo(char* file){
+    char input[100];
+    int fd = open(file, O_WRONLY);
+    fgets(input, 100, stdin);
+    write(fd, input, 100);
+    close(fd);
+}
+  
 
 int main(int argc, char** argv){
     int fd;
@@ -41,12 +32,28 @@ int main(int argc, char** argv){
     (void)umask(0);
     char *n1 = "1";
     char *n0 = "0";
+    mknod(name01, S_IFIFO | 0666, 0);
+    mknod(name10, S_IFIFO | 0666, 0);
+
     if (strcmp(argv[1], n0) == 0){            
-        Dia(name10, name01, fd);
+        pid_t frk = fork();
+        for(;;){
+        if (frk > 0)
+            WriteFifo(name01);
+        else
+            ReadFifo(name10);
+        }
     }
     
     if (strcmp(argv[1], n1) == 0){
-        Dia(name01, name10, fd);
+        pid_t pid = fork();
+        for(;;){
+        if (pid > 0)
+            WriteFifo(name10);
+        else
+            ReadFifo(name01);
+        }
     }
-return 0;
+    close(fd);
+    return 0;
 }
